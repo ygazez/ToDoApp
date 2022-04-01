a2d16078f5bbe8eea1969e258d65ddc7382c17f3
Yağmur Gazezoğlu <yagmur-498@hotmail.com>
2022-04-01 10:43:19 +0300

    last commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reducer,{initialState } from'./context/reducer';
-import {TodoLayer} from'./context/TodoContext';
+
 ReactDOM.render(
   <React.StrictMode>
-    <TodoLayer initialState={initialState} reducer={reducer}>
+   
     <App />
-    </TodoLayer>
+   
     
   </React.StrictMode>,
   document.getElementById('root')
